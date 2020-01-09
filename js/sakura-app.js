7545840460551eb864b9121f3b883332d849c391
yuzhechuan <yuzhechuan@qq.com>
2020-01-09 20:14:26 +0800

    Site updated: 2020-01-09 20:14:24

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -180,10 +180,10 @@ function imgError (ele, type) {
       break
     case 3:
 
-      ele.src = 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/image-404.png'
+      ele.src = 'https://cdn.jsdelivr.net/gh/yuzhechuan/cdn@1.33/img/post/535.webp'
       break
     default:
-      ele.src = 'https://cdn.jsdelivr.net/gh/honjun/cdn@1.6/img/other/image-404.png'
+      ele.src = 'https://cdn.jsdelivr.net/gh/yuzhechuan/cdn@1.33/img/post/535.webp'
   }
 }
 mashiro_global.post_list_show_animation = new function () {
